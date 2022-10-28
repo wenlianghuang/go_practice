166d468bec3d472b8dc18514441d1f9e76a229a7
wenlianghuang <wenliangmatt@gmail.com>
2022-10-28 23:25:05 +0800

    modified files

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "github.com/username/myproject/receiverex"
+import (
+	"github.com/username/myproject/goflags"
+)
 
 func main() {
 	//greeting.Say("Hello World")
 	//goroutine.Goroutine()
 	//deferexample.Deferexample()
 	//mapstruct.MapStruct()
-	receiverex.Receiver()
+	//receiverex.Receiver()
+	goflags.Goflags()
 }
