2c4550641006e0d2c3f98013c732d8d47113a7ae
wenlianghuang <wenliangmatt@gmail.com>
2023-12-06 21:45:20 +0800

    interface

--- interface_practice/main.go
+++ interface_practice/main.go
@@ -4,7 +4,7 @@ package main
 import "interfacepractice/interfacefolder"
 
 func main() {
-	//interfacefolder.Interfacevalue()
+	interfacefolder.Interfacevalue()
 	//interfacefolder.Interfacetype()
-	interfacefolder.Reflectex()
+	//interfacefolder.Reflectex()
 }
